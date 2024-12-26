1afc375a7e138e53d0066c1cc89e86a1ddd0da78
Sungyu Hwang <sun30126331@gmail.com>
2024-12-26 12:17:19 +0900

    [Bronze II] Title: 블랙잭, Time: 428 ms, Memory: 38784 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -6,7 +6,8 @@ for i in range(0, len(card)):
     for j in range(1, len(card)):
         for k in range(0, len(card)):
             if card[i] != card[j] and card[j] != card[k] and card[i] != card[k]:
-                if card[i]+card[j]+card[k] <= m:
-                    result.append(card[i]+card[j]+card[k])
+                if card[i] < card[j] and card[j] < card[k]:
+                    if card[i]+card[j]+card[k] <= m:
+                        result.append(card[i]+card[j]+card[k])
             
 print(max(result))
