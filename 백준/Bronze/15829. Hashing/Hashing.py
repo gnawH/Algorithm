57c8bf7f06679689e243b1c27e690cd03895e762
Sungyu Hwang <sun30126331@gmail.com>
2024-12-28 14:51:48 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,14 +1,10 @@
 L = int(input())
 eng = list(input())
-dict = {}
 result = 0
 tmp = 0
 
-for i in range(1, 27):
-    dict[chr(i+96)] = i
-
 for j in eng:
-    result = result + dict[j] * 31**tmp
-    tmp+=1
+    result += (ord(j)-96) * 31**tmp
+    tmp += 1
 
-print(result)
+print(result % 1234567891)
