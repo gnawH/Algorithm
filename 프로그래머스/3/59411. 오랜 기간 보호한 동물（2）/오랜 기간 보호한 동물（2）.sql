6f438808ae62b5534be931af5f2494718f989a1b
Sungyu Hwang <sun30126331@gmail.com>
2025-02-25 03:35:50 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT B.ANIMAL_ID, B.NAME
+    FROM ANIMAL_INS A
+    JOIN ANIMAL_OUTS B
+    ON A.ANIMAL_ID = B.ANIMAL_ID
+        # WHERE (B.DATETIME-A.DATETIME) =
+            # (SELECT MAX(B.DATETIME-A.DATETIME)
+            #     FROM ANIMAL_INS A
+            #     JOIN ANIMAL_OUTS B  
+            #     ON A.ANIMAL_ID = B.ANIMAL_ID
+            #         GROUP BY B.ANIMAL_ID)
+            ORDER BY (B.DATETIME-A.DATETIME) DESC LIMIT 2;
