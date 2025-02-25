17b20576cf59a2b830bb7ffe1bf443c63730f931
Sungyu Hwang <sun30126331@gmail.com>
2025-02-26 03:14:51 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,2 @@
 def solution(array, commands):
-    answer = []
-    # [1, 5, 2, 6, 3, 7, 4]
-    for c in commands:
-        tmp = []
-        i, j, k = c[0], c[1], c[2]  # 2, 5, 3
-        for a in range(i-1, j):
-            tmp.append(array[a])
-        tmp.sort()
-        answer.append(tmp[k-1])
-    return answer
+    return [sorted(array[i-1:j])[k-1] for i, j, k in commands]
