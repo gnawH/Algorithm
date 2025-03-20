b3116664775c9262f9071685b5325cd0fba4210c
Sungyu Hwang <sun30126331@gmail.com>
2025-03-20 14:20:15 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,3 +1,2 @@
 def solution(my_string, overwrite_string, s):
-    # return(my_string.replace(my_string[s:s+len(overwrite_string)], overwrite_string))
     return(my_string[:s]+overwrite_string+my_string[s+len(overwrite_string):])
