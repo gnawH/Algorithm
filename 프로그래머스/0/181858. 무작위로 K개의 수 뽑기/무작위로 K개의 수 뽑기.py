2444a357b89c9d7d8815d15ceba5049187e98802
Sungyu Hwang <sun30126331@gmail.com>
2025-04-06 00:54:59 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 2476.87 ms, Memory: 9.84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,10 @@
+def solution(arr, k):
+    result = []
+    for i in arr:
+        if i not in result:
+            result.append(i)
+    
+    while len(result) < k:
+        result.append(-1)
+        
+    return result[:k]
