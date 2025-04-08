d135d83c33c32e5076ba296b0b9d48a7a34f3c4f
Sungyu Hwang <sun30126331@gmail.com>
2025-04-08 11:30:43 +0900

    [Silver II] Title: DFS와 BFS, Time: 308 ms, Memory: 35408 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+# 깊이 우선 탐색
+def DFS(v):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            DFS(i)
+
+def BFS(v):
+    queue = deque([v])
+    visited[v] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+        
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n+1)]    # 연결된 노드
+visited = [False] * (n+1)
+
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 정렬
+for i in graph:
+    i.sort()
+
+DFS(v)
+
+# 방문 초기화
+visited = [False] * (n+1)
+print()
+
+BFS(v)
