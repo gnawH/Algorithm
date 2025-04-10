22c109c068a217423e63f02a1ab0d3e8c86b919c
Sungyu Hwang <sun30126331@gmail.com>
2025-04-10 16:59:16 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -6,6 +6,6 @@ def solution(rank, attendance):
             tmp.append((rank[i], i))
             
     s_tmp = sorted(tmp)
-    return (s_tmp[0][1] * 10000 + s_tmp[1][1] * 100 + s_tmp[2][1] * 1)
+    return (s_tmp[0][1] * 10000 + s_tmp[1][1] * 100 + s_tmp[2][1])
         
         
