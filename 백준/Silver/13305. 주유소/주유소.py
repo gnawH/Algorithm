935ebb819a4c49d946c0196e953a356b5cd8f9b1
Sungyu Hwang <sun30126331@gmail.com>
2025-04-19 17:15:26 +0900

    [Silver III] Title: 주유소, Time: 108 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -6,14 +6,11 @@ dis = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
 total = 0
+min_price = price[0]
 
 for i in range(n-1):
-    if price[i] >= price[i+1]:
-        total += (price[i] * dis[i])
-    else:
-        if dis[-1] == dis[i]:
-            break
-        total += (price[i] * (dis[i] + dis[i+1])) 
-        if price[i+1]:
-            price[i+1] = 0
+    if min_price > price[i]:
+        min_price = price[i]
+        
+    total += (min_price * dis[i])
 print(total)
