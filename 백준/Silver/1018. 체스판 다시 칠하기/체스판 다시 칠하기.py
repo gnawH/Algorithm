da9d0adecb2beead1351dde4f46486b1e76c0702
Sungyu Hwang <sun30126331@gmail.com>
2025-11-15 16:35:53 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,26 +1,33 @@
 N, M = map(int, input().split())
-original = []
-count = []
+board = []
 
-for _ in range(N):
-    original.append(input())
+for i in range(N):
+    tmp = []
+    line = input()
+    for j in range(M):
+        tmp += line[j]
+    board.append(tmp)
 
-for a in range(N-7):
-    for b in range(M-7):
-        index1 = 0
-        index2 = 0
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
-                        index1 += 1
-                    if original[i][j] != 'B':
-                        index2 += 1
-                else:
-                    if original[i][j] != 'B':
-                        index1 += 1
-                    if original[i][j] != 'W':
-                        index2 += 1
-        count.append(min(index1, index2))
+test = {True: 'W', False: 'B'}
 
-print(min(count))
+result = []
+
+for height in range(N-7):
+    for width in range(M-7):
+        count1 = 0
+        count2 = 0
+        if board[height][width] == 'W':
+            switch = True
+        elif board[height][width] == 'B':
+            switch = False
+        for i in range(height, height+8): # 10 9 8
+            switch = not switch
+            for j in range(width, width+8):
+                if board[i][j] != test[switch]:
+                    count1 += 1
+                elif board[i][j] != test[not switch]:
+                    count2 += 1
+                switch = not switch
+        result.append(count1)
+        result.append(count2)
+print(min(result))
