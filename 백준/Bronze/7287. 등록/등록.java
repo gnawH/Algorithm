ec8e8459229d0edde2e66f0c27789d4b06843ec3
Sungyu Hwang <sun30126331@gmail.com>
2026-01-13 18:07:32 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13968 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -1,6 +1,7 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println(140);
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("141");
         System.out.println("sun011018");
     }
 }
