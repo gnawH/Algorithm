92ee2fa254a2bc3c6911fef36a0c73827d1e66ac
Sungyu Hwang <sun30126331@gmail.com>
2026-01-16 08:39:31 +0900

    [Silver III] Title: 1로 만들기, Time: 188 ms, Memory: 21616 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -5,18 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] dp = new int[1000001];
         int N = sc.nextInt();
+        int[] dp = new int[1000001];
 
         for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
-
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i/2] + 1);
-            }
+            dp[i] = dp[i-1] + 1;
 
             if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i/3] + 1);
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
         }
         System.out.println(dp[N]);
