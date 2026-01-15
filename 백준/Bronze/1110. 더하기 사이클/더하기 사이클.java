c9e3a55c2155073717112d9cfb64aacbdf714a21
Sungyu Hwang <sun30126331@gmail.com>
2026-01-16 08:14:41 +0900

    [Bronze I] Title: 더하기 사이클, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -6,25 +6,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int number = sc.nextInt();
-        int origin_num = number;
+        int tmp = number;
+
         int left, right = 0;
         int result = -1;
         int count = 0;
 
-        while (origin_num != result) {
-            // 10 이하 수, 전처리
-            if (number < 10) {
-                left = 0;
-            } else {
-                left = number / 10;
-            }
-
-            right = number % 10;
-
+        do {
+            left = tmp / 10;
+            right = tmp % 10;
             result = (right * 10) + ((left + right) % 10);
+
+            tmp = result;
             count ++;
-            number = result;
-        }
+        } while (number != result);
+
         System.out.println(count);
     }
 }
