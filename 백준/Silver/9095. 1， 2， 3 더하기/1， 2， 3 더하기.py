9d4579d34129318e3b67eef1ceb52ba02d51c66c
Sungyu Hwang <sun30126331@gmail.com>
2026-01-18 23:38:31 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+# dp
+dp = [0] * 11
+dp[1] = 1   # 1
+dp[2] = 2   # 2, 1+1
+dp[3] = 4   # 3, 2+1, 1+2, 1+1+1
+
+for _ in range(T):
+    n = int(input())
+
+    for i in range(4, n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    print(dp[n])
