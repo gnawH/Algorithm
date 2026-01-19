3a7038e00ccd7c100934dfae10c915790a97c5ca
Sungyu Hwang <sun30126331@gmail.com>
2026-01-20 01:15:59 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -3,11 +3,9 @@ T = int(input())
 P = [0] * 101
 P[1:11] = [1, 1, 1, 2, 2, 3, 4, 5, 7, 9]
 
+for i in range(11, 101):
+    P[i] = P[i - 5] + P[i - 1]
+
 for test_case in range(T):
     N = int(input())
-
-    if N > 10:
-        for i in range(10, N+1):
-            P[i] = P[i-5] + P[i-1]
-
     print(P[N])
