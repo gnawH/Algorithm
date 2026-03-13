6341c9cae244b0178899a9773498814f6cc8ab70
Sungyu Hwang <sun30126331@gmail.com>
2026-03-13 12:39:56 +0900

    [Silver I] Title: 신입 사원, Time: 5168 ms, Memory: 73176 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -5,17 +5,20 @@ T = int(input())
 
 for test_case in range(T):
     N = int(input())
-    scores = [list(map(int, input().split())) for _ in range(N)]
+    ranks = [list(map(int, input().split())) for _ in range(N)]
     count = 0
 
     # 1차 점수 기준 정렬
-    scores.sort()
-    standard_score = N+1
+    ranks.sort()
+    r_stack = []
 
-    for st, nd in scores:
-        if standard_score > nd:
-            standard_score = nd
-            count += 1
+    for apply in ranks:
+        if len(r_stack) == 0:
+            r_stack.append(apply)
         else:
-            continue
-    print(count)
+            if r_stack[-1][1] > apply[1]:
+                r_stack.append(apply)
+            else:
+                continue
+
+    print(len(r_stack))
